17c9f34fc4ae6528bc94bb5271d32c425cc699fb
tsgouros <tsgouros@gmail.com>
2017-03-17 08:43:59 -0400

    made demo light color white

--- src/textureDemo.cpp
+++ src/textureDemo.cpp
@@ -273,7 +273,7 @@ int main(int argc, char **argv) {
   // lighting, and the shapes don't have textures, this is irrelevant.
   bsg::bsgPtr<bsg::lightList> lights = new bsg::lightList();
   lights->addLight(glm::vec4(0.0f, 0.0f, 3.0f, 1.0f),
-                   glm::vec4(1.0f, 0.0f, 0.0f, 0.0f));
+                   glm::vec4(1.0f, 1.0f, 1.0f, 0.0f));
 
   // Now we load the shaders.  First check to see if any have been
   // specified on the command line.
